c1e8c04a7f518c39351f6360aea70d3257dbf6dc
Jan Masarik <cambion9999@gmail.com>
2019-05-05 14:48:56 +0200

    Fix Oracle

--- bucketsperm/modules/oracle.py
+++ bucketsperm/modules/oracle.py
@@ -10,9 +10,12 @@ class Oracle(BaseWorker):
     references = []
 
     def run(self):
-        regions = ["us-phoenix-1", "us-ashburn-1", "uk-london-1", "eu-frankfurt-1"]
+        regions = ["eu-frankfurt-1", "uk-london-1", "us-phoenix-1", "us-ashburn-1"]
         for region in regions:
-            bucket_url = f"https://compat.objectstorage.{region}.oraclecloud.com/n/{self.namespace}/b/{self.bucket_name}"
+            if region == "uk-london-1":
+                bucket_url = f"https://{self.namespace}.compat.objectstorage.{region}.oraclecloud.com/{self.bucket_name}"
+            else:
+                bucket_url = f"https://compat.objectstorage.{region}.oraclecloud.com/n/{self.namespace}/b/{self.bucket_name}"
             r = requests.get(f"{bucket_url}/{self.random_string()}")
             if "NoSuchKey" in r.text:
                 break
@@ -32,11 +35,11 @@ class Oracle(BaseWorker):
         # https://docs.cloud.oracle.com/iaas/Content/Object/Tasks/managingbuckets.htm
         if len(bucket_name) > 256:
             return False
-        
+
         allowed_chars = set(string.digits + string.ascii_letters + "-_.")
-        
+
         if any(c not in allowed_chars for c in bucket_name):
             return False
 
         return True
-        
+
